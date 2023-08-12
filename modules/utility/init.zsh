86236ac11e4aa15d664f826e363109f65b49dc05
Rahul Ahuja <github@rahuja.net>
2023-08-12 16:58:10 -0500

    Add customizations

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -22,52 +22,52 @@ autoload -Uz run-help-{ip,openssl,sudo}
 # Aliases
 #
 
-# Disable correction.
-alias ack='nocorrect ack'
-alias cd='nocorrect cd'
-alias cp='nocorrect cp'
-alias ebuild='nocorrect ebuild'
-alias gcc='nocorrect gcc'
-alias gist='nocorrect gist'
-alias grep='nocorrect grep'
-alias heroku='nocorrect heroku'
-alias ln='nocorrect ln'
-alias man='nocorrect man'
-alias mkdir='nocorrect mkdir'
-alias mv='nocorrect mv'
-alias mysql='nocorrect mysql'
-alias rm='nocorrect rm'
+# # Disable correction.
+# alias ack='nocorrect ack'
+# alias cd='nocorrect cd'
+# alias cp='nocorrect cp'
+# alias ebuild='nocorrect ebuild'
+# alias gcc='nocorrect gcc'
+# alias gist='nocorrect gist'
+# alias grep='nocorrect grep'
+# alias heroku='nocorrect heroku'
+# alias ln='nocorrect ln'
+# alias man='nocorrect man'
+# alias mkdir='nocorrect mkdir'
+# alias mv='nocorrect mv'
+# alias mysql='nocorrect mysql'
+# alias rm='nocorrect rm'
 
-# Disable globbing.
-alias bower='noglob bower'
-alias fc='noglob fc'
-alias find='noglob find'
-alias ftp='noglob ftp'
-alias history='noglob history'
-alias locate='noglob locate'
-alias rake='noglob rake'
-alias rsync='noglob rsync'
-alias scp='noglob scp'
-alias sftp='noglob sftp'
+# # Disable globbing.
+# alias bower='noglob bower'
+# alias fc='noglob fc'
+# alias find='noglob find'
+# alias ftp='noglob ftp'
+# alias history='noglob history'
+# alias locate='noglob locate'
+# alias rake='noglob rake'
+# alias rsync='noglob rsync'
+# alias scp='noglob scp'
+# alias sftp='noglob sftp'
 
-# Define general aliases.
-alias _='sudo'
-alias b='${(z)BROWSER}'
+# # Define general aliases.
+# alias _='sudo'
+# alias b='${(z)BROWSER}'
 
-alias diffu="diff --unified"
-alias e='${(z)VISUAL:-${(z)EDITOR}}'
-alias mkdir="${aliases[mkdir]:-mkdir} -p"
-alias p='${(z)PAGER}'
-alias po='popd'
-alias pu='pushd'
-alias sa='alias | grep -i'
-alias type='type -a'
+# alias diffu="diff --unified"
+# alias e='${(z)VISUAL:-${(z)EDITOR}}'
+# alias mkdir="${aliases[mkdir]:-mkdir} -p"
+# alias p='${(z)PAGER}'
+# alias po='popd'
+# alias pu='pushd'
+# alias sa='alias | grep -i'
+# alias type='type -a'
 
-# Safe ops. Ask the user before doing anything destructive.
-alias cpi="${aliases[cp]:-cp} -i"
-alias lni="${aliases[ln]:-ln} -i"
-alias mvi="${aliases[mv]:-mv} -i"
-alias rmi="${aliases[rm]:-rm} -i"
+# # Safe ops. Ask the user before doing anything destructive.
+# alias cpi="${aliases[cp]:-cp} -i"
+# alias lni="${aliases[ln]:-ln} -i"
+# alias mvi="${aliases[mv]:-mv} -i"
+# alias rmi="${aliases[rm]:-rm} -i"
 if zstyle -T ':prezto:module:utility' safe-ops; then
   alias cp="${aliases[cp]:-cp} -i"
   alias ln="${aliases[ln]:-ln} -i"
