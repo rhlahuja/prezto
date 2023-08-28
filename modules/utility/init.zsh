1f6bf280c168623ebb82eb6da0a5b51d1a1e2d20
Rahul Ahuja <github@rahuja.net>
2023-08-28 14:11:45 -0500

    Disable unused aliases

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -113,20 +113,20 @@ else
   fi
 fi
 
-alias l='ls -1A'         # Lists in one column, hidden files.
-alias ll='ls -lh'        # Lists human readable sizes.
-alias lr='ll -R'         # Lists human readable sizes, recursively.
-alias la='ll -A'         # Lists human readable sizes, hidden files.
-alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
-alias lk='ll -Sr'        # Lists sorted by size, largest last.
-alias lt='ll -tr'        # Lists sorted by date, most recent last.
-alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
-alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
-alias sl='ls'            # Correction for common spelling error.
+# alias l='ls -1A'         # Lists in one column, hidden files.
+# alias ll='ls -lh'        # Lists human readable sizes.
+# alias lr='ll -R'         # Lists human readable sizes, recursively.
+# alias la='ll -A'         # Lists human readable sizes, hidden files.
+# alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
+# alias lk='ll -Sr'        # Lists sorted by size, largest last.
+# alias lt='ll -tr'        # Lists sorted by date, most recent last.
+# alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
+# alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
+# alias sl='ls'            # Correction for common spelling error.
 
-if [[ ${(@M)${(f)"$(ls --version 2>&1)"}:#*GNU *} ]]; then
-  alias lx='ll -XB'      # Lists sorted by extension (GNU only).
-fi
+# if [[ ${(@M)${(f)"$(ls --version 2>&1)"}:#*GNU *} ]]; then
+#   alias lx='ll -XB'      # Lists sorted by extension (GNU only).
+# fi
 
 # Grep
 if zstyle -t ':prezto:module:utility:grep' color; then
